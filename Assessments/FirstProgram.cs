8c3b7611bad9c17ca7e5667372bdd3ed0ff20e28
aniketkalvit <aniketkalvit@gmail.com>
2024-04-12 10:24:55 +0530

    Add project files.

---
+++ Assessments/FirstProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Assessments
+{
+    public class FirstProgram
+    {
+        // main is not public method
+        // Main () M is in capital letter
+        static void Main(string[] args)
+        {
+            int a = 30, b = 20;
+           bool result= (a<=b)? true:false;
+
+            Console.WriteLine(result);
+
+            int result2 = (a <= b) ? a : b;
+            Console.WriteLine(result2);
+
+            string result3 = (a <= b) ? "a is less than b" : "a is greater than b";
+            Console.WriteLine(result3);
+            
+                      
+        }
+    }
+    
+}
+
+namespace Test1
+{
+
+    public class A
+    {
+
+    }
+}
+
+namespace Test2
+{
+
+    public class A
+    {
+
+    }
+}
---
+++ Assessments/Program.cs
@@ -0,0 +1,22 @@
+﻿// single line comment
+
+/*
+ multiline comment in C#
+ */
+
+// accept the value from user --> Console.ReadLine() --> return string
+
+//Console.WriteLine("Enter your name");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Enter your age");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter your salary");
+//double salary = Convert.ToDouble(Console.ReadLine());
+
+////Console.WriteLine("Hello "+name);
+//Console.WriteLine($"Hello {name} , your age is {age} and salary is {salary}");
+
+
+
